039e9d3e8e868f6599caabcfcf0fbc94596ed34b
nhnajmul17 <nhnajmul17@gmail.com>
2021-10-01 14:53:28 +0600

    first commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css'
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
+    //products to be render on UI
+    const [displayProducts, setDisplayProducts] = useState([]);
+
+
+    useEffect(() => {
+        fetch('./products.JSON')
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data);
+                setDisplayProducts(data)
+            })
+
+    }, [])
+
+    useEffect(() => {
+        if (products.length) {
+            const savedCart = getStoredCart();
+            const storedCart = []
+
+            for (const key in savedCart) {
+                const addedProduct = products.find(product => product.key === key);
+                if (addedProduct) {
+                    const quantity = savedCart[key];
+                    addedProduct.quantity = quantity;
+                    storedCart.push(addedProduct);
+
+                }
+            }
+            setCart(storedCart)
+        }
+
+    }, [products])
+
+
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+        //save to local storage for now 
+        addToDb(product.key);
+
+    }
+
+    const handleSearch = event => {
+        const searchText = event.target.value;
+        const matchProducts = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
+        setDisplayProducts(matchProducts);
+
+    }
+    return (
+        <div>
+            <div className='search-container'>
+                <input type="text"
+                    onChange={handleSearch}
+                    placeholder='search product' />
+
+            </div>
+
+
+            <div className='shop-container'>
+                <div className="product-container">
+                    {
+                        displayProducts.map(product => <Product
+                            key={product.key}
+                            product={product}
+                            handleAddToCart={handleAddToCart}>
+
+                        </Product>)
+                    }
+                </div>
+
+                <div >
+                    <Cart cart={cart}></Cart>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
